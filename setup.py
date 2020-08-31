20dbb618cc8d67b0f46176676741bb015de71d7b
Akshit Ahuja <ahujaakshit20@gmail.com>
2020-08-31 09:07:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
+import io
+import os
+current = os.path.realpath(os.path.dirname(__file__))
+with io.open(os.path.join(current, 'README.md'), encoding="utf-8") as f:
+    long_description = f.read()
 setup(
     name='climailsystem',
-    version='0.0',
+    version='0.11',
     author = 'Akshit Ahuja',
     author_email = "techsyapa@gmail.com",
     description = "A small Package for sending  mails from cli",
     url = "https://github.com/aksh45/climailsystem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License  :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=['sendmail'],
